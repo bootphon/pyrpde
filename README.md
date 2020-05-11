c10efecbb370699a61b10aab3723396f341c364f
hadware <hadwarez@gmail.com>
2020-05-11 12:22:28 +0200

    Updated API to resemble a numpy/scipy function, added docstrings. Updated README.md

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # pyRPDE
 
-A full-python implementation of the Recurrence Period Density Entropy metric
+A full-python implementation of the Recurrence Period Density Entropy metric.
+It's based on the algorithm described in [1], and based on Max Little's 
+R (and C) implementation in [2].
 
 ## Installation
 
-This package is available on pypi, so a simple
+This package needs python >= 3.6, and relies on Numba, Scipy, and Numpy.
+It's available on pypi, so a simple:
 
 ```shell
 pip install pyrpde
 ```
 
-should do the trick
+should do the trick.
 
 ## Usage
 
--- pyrpde/__init__.py
+++ pyrpde/__init__.py
@@ -1,131 +1,159 @@
+from typing import Optional, Tuple
+
 import numpy as np
 import numba as nb
 from scipy.stats import entropy
 
-# TODO : change to a scipy-style function that outputs the entropy and the recurrence histogram
 
-class RPDE:
-    def __init__(self,
-                 dim: int,
-                 tau: int,
-                 epsilon: float,
-                 tmax: Optional[int] = None,
-                 parallel: bool = True):
-        super().__init__(dim=dim, tau=tau, epsilon=epsilon, tmax=tmax)
-        self.epsilon, self.tau, self.dim = epsilon, tau, dim
-        self.tmax = tmax
-        self.parallel = parallel
+@nb.jit(nb.int32[:](nb.float32[:, :], nb.float32, nb.int32), nopython=True)
+def recurrence_histogram(ts: np.ndarray, epsilon: float, t_max: int):
+    """Numba implementation of the recurrence histogram described in
+    http://www.biomedical-engineering-online.com/content/6/1/23
 
-    @staticmethod
-    @nb.jit(nb.int32[:](nb.float32[:, :], nb.float32, nb.int32), nopython=True)
-    def recurrence_histogram(ts: np.ndarray, epsilon: float, t_max: int):
-        """Numba implementation of the recurrence histogram described in
-        http://www.biomedical-engineering-online.com/content/6/1/23
+    :param ts: Time series of dimension (T,N)
+    :param epsilon: Recurrence ball radius
+    :param t_max: Maximum distance for return.
+    Larger distances are not recorded. Set to -1 for infinite distance.
+    """
+    return_distances = np.zeros(len(ts), dtype=np.int32)
+    for i in np.arange(len(ts)):
+        # finding the first "out of ball" index
+        first_out = len(ts)  # security
+        for j in np.arange(i + 1, len(ts)):
+            if 0 < t_max < j - i:
+                break
+            d = np.linalg.norm(ts[i] - ts[j])
+            if d > epsilon:
+                first_out = j
+                break
 
-        :param ts: Time series of dimension (T,N)
-        :param epsilon: Recurrence ball radius
-        :param t_max: Maximum distance for return.
-        Larger distances are not recorded. Set to -1 for infinite distance.
-        """
-        return_distances = np.zeros(len(ts), dtype=np.int32)
-        for i in np.arange(len(ts)):
-            # finding the first "out of ball" index
-            first_out = len(ts)  # security
-            for j in np.arange(i + 1, len(ts)):
-                if 0 < t_max < j - i:
-                    break
-                d = np.linalg.norm(ts[i] - ts[j])
-                if d > epsilon:
-                    first_out = j
-                    break
+        # finding the first "back to the ball" index
+        for j in np.arange(first_out + 1, len(ts)):
+            if 0 < t_max < j - i:
+                break
+            d = np.linalg.norm(ts[i] - ts[j])
+            if d < epsilon:
+                return_distances[j - i] += 1
+                break
+    return return_distances
 
-            # finding the first "back to the ball" index
-            for j in np.arange(first_out + 1, len(ts)):
-                if 0 < t_max < j - i:
-                    break
-                d = np.linalg.norm(ts[i] - ts[j])
-                if d < epsilon:
-                    return_distances[j - i] += 1
-                    break
-        return return_distances
 
-    @staticmethod
-    @nb.jit(nb.int32[:](nb.float32[:, :], nb.float32, nb.int32), parallel=True, nopython=True)
-    def parallel_recurrence_histogram(ts: np.ndarray, epsilon: float,
-                                      t_max: int):
-        """Parallelized implem of the recurrence histogram. Works the same,
-        but adapted to parallelized computing (automatically done by Numba)."""
-        return_distances = np.zeros(len(ts), dtype=np.int32)
+@nb.jit(nb.int32[:](nb.float32[:, :], nb.float32, nb.int32), parallel=True, nopython=True)
+def parallel_recurrence_histogram(ts: np.ndarray, epsilon: float,
+                                  t_max: int):
+    """Parallelized implem of the recurrence histogram. Works the same,
+    but adapted to parallelized computing (automatically done by Numba)."""
+    return_distances = np.zeros(len(ts), dtype=np.int32)
 
-        # this is the parallized loop
-        for i in nb.prange(len(ts)):
-            # finding the first "out of ball" index
-            first_out = len(ts)  # security
-            for j in np.arange(i + 1, len(ts)):
-                if t_max > 0 and j - i > t_max:
-                    break
-                d = np.linalg.norm(ts[i] - ts[j])
-                if d > epsilon:
-                    first_out = j
-                    break
+    # this is the parallized loop
+    for i in nb.prange(len(ts)):
+        # finding the first "out of ball" index
+        first_out = len(ts)  # security
+        for j in np.arange(i + 1, len(ts)):
+            if t_max > 0 and j - i > t_max:
+                break
+            d = np.linalg.norm(ts[i] - ts[j])
+            if d > epsilon:
+                first_out = j
+                break
 
-            # finding the first "back to the ball" index
-            for j in np.arange(first_out + 1, len(ts)):
-                if t_max > 0 and j - i > t_max:
-                    break
-                d = np.linalg.norm(ts[i] - ts[j])
-                if d < epsilon:
-                    return_distances[i] = j - i
-                    break
+        # finding the first "back to the ball" index
+        for j in np.arange(first_out + 1, len(ts)):
+            if t_max > 0 and j - i > t_max:
+                break
+            d = np.linalg.norm(ts[i] - ts[j])
+            if d < epsilon:
+                return_distances[i] = j - i
+                break
 
-        # building histogram, can't be parallel
-        # (potential concurrent access to histogram items)
-        distance_histogram = np.zeros(len(ts), dtype=np.int32)
-        for i in np.arange(len(ts)):
-            if return_distances[i] != 0:
-                distance_histogram[return_distances[i]] += 1
-        return distance_histogram
+    # building histogram, can't be parallel
+    # (potential concurrent access to histogram items)
+    distance_histogram = np.zeros(len(ts), dtype=np.int32)
+    for i in np.arange(len(ts)):
+        if return_distances[i] != 0:
+            distance_histogram[return_distances[i]] += 1
+    return distance_histogram
 
-    @staticmethod
-    def embed_time_series(data: np.ndarray, dim: int, tau: int):
-        """Embeds the time series, tested against the pyunicorn implementation
-        of that transform"""
-        # TODO : check that this implem holds when the data shape isn't unidimentional
-        embed_points_nb = data.shape[0] - (dim - 1) * tau
-        # embed_mask is of shape (embed_pts, dim)
-        embed_mask = np.arange(embed_points_nb)[:, np.newaxis] + np.arange(dim)
-        tau_offsets = np.arange(dim) * (tau - 1)  # shape (dim,1)
-        embed_mask = embed_mask + tau_offsets
-        return data[embed_mask]
 
-    def __call__(self, time_series: np.ndarray) -> float:
-        # converting the sound array to the right format
-        # (RPDE expects the array to be floats in [-1,1]
-        # TODO: check DType
-        if sample_data.array.dtype == np.int16:
-            data = (sample_data.array / (2**16)).astype(np.float32)
-        else:
-            data = sample_data.array.astype(np.float32)
+def embed_time_series(data: np.ndarray, dim: int, tau: int):
+    """Embeds the time series, tested against the pyunicorn implementation
+    of that transform"""
+    # TODO : check that this implem holds when the data shape isn't unidimentional
+    embed_points_nb = data.shape[0] - (dim - 1) * tau
+    # embed_mask is of shape (embed_pts, dim)
+    embed_mask = np.arange(embed_points_nb)[:, np.newaxis] + np.arange(dim)
+    tau_offsets = np.arange(dim) * (tau - 1)  # shape (dim,1)
+    embed_mask = embed_mask + tau_offsets
+    return data[embed_mask]
 
-        #  building the time series, and computing the recurrence histogram
-        embedded_ts = self.embed_time_series(data, self.dim, self.tau)
-        if self.tmax is None:
-            rec_histogram = self.parallel_recurrence_histogram(
-                embedded_ts, self.epsilon, -1)
-            # tmax is the highest non-zero value in the histogram
-            tmax_idx = np.argwhere(rec_histogram != 0).flatten()[-1]
-        else:
-            rec_histogram = self.parallel_recurrence_histogram(
-                embedded_ts, self.epsilon, self.tmax)
-            tmax_idx = self.tmax
-        # culling the histogram at the tmax index
-        culled_histogram = rec_histogram[:tmax_idx]
 
-        # normalizing the histogram (to make it a probability)
-        # and computing its entropy
-        if culled_histogram.sum():
-            normalized_histogram = culled_histogram / culled_histogram.sum()
-            histogram_entropy = entropy(normalized_histogram)
-            return histogram_entropy / np.log(culled_histogram.size)
-        else:
-            return 0.0
+def rpde(time_series: np.ndarray,
+         dim: int = 4,
+         tau: int = 32,
+         epsilon: float = 0.12,
+         tmax: Optional[int] = None,
+         parallel: bool = True) -> Tuple[float, np.ndarray]:
+    """
+
+    Parameters
+    ----------
+    time_series: np.ndarray
+        The input time series. Had to be floats, normalized to [-1,1]
+    dim: int
+        The dimension of the time series embeddings. Defaults to 4
+    tau: int
+        The "stride" between each of the embedding points in a time series'
+        embedding vector. Defaults to 32
+    epsilon: float
+        The size of the unit ball described in the RPDE algorithm.
+        Defaults to 0.12.
+    tmax: int, optional
+        Maximum return distance (n1-n0), return distances higher than this
+        are ignored. Defaults to None.
+    parallel: boolean, optional
+        Use the parallelized Numba implementation. The parallelization overhead
+        might make this slower in certain situations. Defaults to True.
+
+    Returns
+    -------
+    rpde: float
+        Value of the RPDE
+    histogram: np.ndarray
+        1-dimensional array corresponding to the histogram of the return
+        distances
+    """
+
+    # (RPDE expects the array to be floats in [-1,1]
+    if not time_series.dtype == np.float:
+        raise ValueError("Time series should be floats")
+
+    if not (np.abs(time_series) <= 1.0).all():
+        raise ValueError("The time series' values have to be normalized "
+                         "to [-1, 1]")
+
+    #  building the time series, and computing the recurrence histogram
+    embedded_ts = embed_time_series(time_series, dim, tau)
+    
+    if parallel:
+        histogram_fn = parallel_recurrence_histogram
+    else:
+        histogram_fn = recurrence_histogram
+    
+    if tmax is None:
+        rec_histogram = histogram_fn(embedded_ts, epsilon, -1)
+        # tmax is the highest non-zero value in the histogram
+        tmax_idx = np.argwhere(rec_histogram != 0).flatten()[-1]
+    else:
+        rec_histogram = histogram_fn(embedded_ts, epsilon, tmax)
+        tmax_idx = tmax
+    # culling the histogram at the tmax index
+    culled_histogram = rec_histogram[:tmax_idx]
+
+    # normalizing the histogram (to make it a probability)
+    # and computing its entropy
+    if culled_histogram.sum():
+        normalized_histogram = culled_histogram / culled_histogram.sum()
+        histogram_entropy = entropy(normalized_histogram)
+        return histogram_entropy / np.log(culled_histogram.size), culled_histogram
+    else:
+        return 0.0, culled_histogram
